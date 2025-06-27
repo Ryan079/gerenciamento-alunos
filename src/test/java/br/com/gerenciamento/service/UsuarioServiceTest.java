b6dbbe5af3951a46338192690a65c4762a56a04a
Ryan079 <ryan079077@gmail.com>
2025-06-27 00:38:55 -0300

    Implementação dos 4 testes para UsuarioService
    Implementação do teste de login bem sucedido em UsuarioServiceTest
    
    Implementação do teste de login com usuário inexistente em UsuarioServiceTest
    
    Implementação do teste de login com senha incorreta em UsuarioServiceTest
    
    Implementação do teste de salvar usuario com nome longo em UsuarioServiceTest

--- src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
+++ src/test/java/br/com/gerenciamento/service/UsuarioServiceTest.java
@@ -1,10 +1,67 @@
 package br.com.gerenciamento.service;
 
+import br.com.gerenciamento.model.Usuario;
+import jakarta.servlet.ServletConfig;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UsuarioServiceTest {
+
+    @Autowired
+    private ServiceUsuario serviceUsuario;
+
+    @Test
+    public void loginBemSucedido() throws Exception {
+        Usuario usuario = new Usuario();
+        usuario.setUser("Ryan");
+        usuario.setSenha("senha987");
+        usuario.setEmail("ryan2@gmail.com");
+
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = serviceUsuario.loginUser(usuario.getUser(), usuario.getSenha());
+
+        Assert.assertEquals(usuario.getEmail(),usuarioLogado.getEmail());
+
+    }
+
+    @Test
+    public void loginUsuarioInexistente() {
+
+        Assert.assertNull(this.serviceUsuario.loginUser("Ryan", "Senha"));
+    }
+
+    @Test
+    public void loginSenhaIncorreta() throws Exception{
+        Usuario usuario = new Usuario();
+        usuario.setUser("Ryan");
+        usuario.setSenha("senha1234");
+        usuario.setEmail("ryan@gmail.com");
+
+        this.serviceUsuario.salvarUsuario(usuario);
+
+        Usuario usuarioLogado = serviceUsuario.loginUser(usuario.getUser(), "SenhaQualquer");
+
+        Assert.assertNull(usuarioLogado);
+
+    }
+
+    @Test
+    public void salvarUsuarioNomeLongo() {
+        Usuario usuario = new Usuario();
+        usuario.setUser("Usuario realmente com nome muito grande mesmo");
+        usuario.setSenha("senha1234");
+        usuario.setEmail("emaildeusuariodenomemuitogrande@gmail.com");
+
+        Assert.assertThrows(Exception.class, () -> {
+            this.serviceUsuario.salvarUsuario(usuario);
+        });
+    }
 }
